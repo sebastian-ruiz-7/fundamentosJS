645165dae4bf9706e7904d39430c91ac92c8258c
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2021-01-23 00:39:19 -0600

    Clase2-Strings en JS

--- clase1.js
+++
@@ -1,7 +0,0 @@
-var nombre='Sebastián', apellido='Ruiz';
-console.log('Hola ' + nombre + ' ' + apellido);
-
-//JS es un lenguaje débilmente tipado. Eso quiere decir que: 
-//Las variables pueden iniciar con un tipo definido y posteriormete durante el programa cambiar de tipo
-//En este caso la variable "nombre" inició como tipo string pero ahora está como tipo number
-nombre=7;
