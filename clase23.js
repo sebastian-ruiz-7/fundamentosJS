981338ae30f5d3636c8ed7a703eb8e4f336817e8
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2021-02-11 16:16:55 -0600

    Se estudió el problema con los this en las arrows funtions

--- clase23.js
+++ clase23.js
@@ -13,19 +13,16 @@ var sebastian = new persona('Sebastián', 'Ruiz',1.7);
 var aldo = new persona('Aldo','Ruiz',1.65);
 var oswaldo =new persona('Oswaldo','Silva',1.81);
 
-//Crear la función para que la persona salude
-
-
-
-//RETO: agrgar la función al prototipo de la persona en el que se diga si la persona es alta o no (>1.8m)
-// persona.prototype.alta=function(){
-//     if(this.altura<1.8){
-//         console.log('No soy alto');
-//     }else{
-//         console.log('Soy alto');
-//     }
-// }
 
 //Sin embargo si hacemos la evaluación de la altura con una arrow funtion se hace desprende un error
 //De este error se explorará en la siguiente clase sobre el contexto de las funciones
-persona.prototype.alta=()=>this.altura>1.8
+
+//Se este 'error' se arregla de la siguiente manera
+//El problema ocurre debido a que el this en las arrow funtion apuntan al objeto window y no al objeto persona
+persona.prototype.alta=function(){
+    if(this.altura<1.8){
+        console.log('No soy alto');
+    }else{
+        console.log('Soy alto');
+    }
+}
